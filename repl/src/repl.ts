492beedc3347d8a715563422bba23c087bed3afe
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-16 17:14:37 +0100

    test undefined

--- repl/src/repl.ts
+++ repl/src/repl.ts
@@ -1,11 +1,11 @@
 import diff from "../../app/src/fastObjectDiff"
 
-console.log(diff.flat({
-  a: "qwe", 
-  nested: 22
-}, {
-  a: "qwe",
-  nested: {e: 123},
-  b: 2
-}))
+console.log(diff.flat(
+  undefined,
+  {
+    a: "qwe",
+    b: 22,
+    aw: {qwe: "qwe"}
+  }
+))
 
